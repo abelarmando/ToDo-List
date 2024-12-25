bf2982f14222f85d83de512d49b9ebe36d9ac8a4
abel armando <abelarmando79@gmail.com>
2024-12-25 10:35:46 +0700

    merapikan css dan menambahkan dropdown list

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-alert('haha')
+'use-strict'
+
+const buttontodo = document.querySelector('.button-todo');
+const form = document.querySelector('.form');
+const overlay = document.querySelector('.overlay');
+
+// function untuk menampilkan / menghilangkan overlay dan form todo
+const openhidden = function() {
+    form.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closehidden = function() {
+    form.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+buttontodo.addEventListener('click', openhidden);
+
+document.addEventListener('keydown', function(e) {
+    if(e.key === 'Escape' && !form.classList.contains('hidden')) {
+        closehidden();
+    }
+    console.log(e.key);
+});
+
